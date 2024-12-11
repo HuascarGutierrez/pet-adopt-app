99ebcf1c6566a7689c611399a88c7a3c6a5b7cd5
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2024-12-11 16:53:25 -0400

    authentication completed

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,37 @@
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SecureStore from 'expo-secure-store'
+import { Platform } from 'react-native'
+
+const createTokenCache = () => {
+  return {
+    getToken: async (key) => {
+      try {
+        const item = await SecureStore.getItemAsync(key)
+        if (item) {
+          console.log(`${key} was used 🔐 \n`)
+        } else {
+          console.log('No values stored under key: ' + key)
+        }
+        return item
+      } catch (error) {
+        console.error('secure store get item error: ', error)
+        await SecureStore.deleteItemAsync(key)
+        return null
+      }
+    },
+    saveToken: (key, token) => {
+      return SecureStore.setItemAsync(key, token)
+    },
+  }
+}
+const tokenCache = Platform.OS !== 'web' ? createTokenCache() : undefined
+
 
 export default function RootLayout() {
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY
+
 
   useFonts({
     'outfit': require('./../assets/fonts/PlaywriteHU-Regular.ttf'),
@@ -11,6 +41,7 @@ export default function RootLayout() {
   })
 
   return (
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
     <Stack>
       <Stack.Screen name="index"/>
       <Stack.Screen name="login/index"
@@ -18,5 +49,6 @@ export default function RootLayout() {
           headerShown: false,
         }}/>
     </Stack>
+    </ClerkProvider>
   );
 }
--- app/login/index.jsx
+++ app/login/index.jsx
@@ -1,8 +1,45 @@
 import { View, Text, Image, Pressable } from 'react-native'
 import React from 'react'
 import Colors from './../../constants/Colors'
+import * as WebBrowser from 'expo-web-browser'
+import * as Linking from 'expo-linking'
+import { useOAuth } from '@clerk/clerk-expo'
+
+export const useWarmUpBrowser = () => {
+    React.useEffect(() => {
+      // Warm up the android browser to improve UX
+      // https://docs.expo.dev/guides/authentication/#improving-user-experience
+      void WebBrowser.warmUpAsync()
+      return () => {
+        void WebBrowser.coolDownAsync()
+      }
+    }, [])
+  }
 
 export default function LoginScreen() {
+    useWarmUpBrowser()
+    const { startOAuthFlow } = useOAuth({ strategy: 'oauth_google' })
+
+    const onPress = React.useCallback(async () => {
+        try {
+          const { createdSessionId, signIn, signUp, setActive } = await startOAuthFlow({
+            redirectUrl: Linking.createURL('/home', { scheme: 'myapp' }),
+          })
+    
+          // If sign in was successful, set the active session
+          if (createdSessionId) {
+            //setActive!({ session: createdSessionId })
+          } else {
+            // Use signIn or signUp returned from startOAuthFlow
+            // for next steps, such as MFA
+          }
+        } catch (err) {
+          // See https://clerk.com/docs/custom-flows/error-handling
+          // for more info on error handling
+          console.error(JSON.stringify(err, null, 2))
+        }
+      }, [])
+
   return (
     <View>
         <Image source={require('./../../assets/images/login.png')} style={{
@@ -26,7 +63,7 @@ export default function LoginScreen() {
             color: Colors.GRAY,
         }}>Let's adopt the pet which you like and make there life happy again</Text>
 
-        <Pressable style={{
+        <Pressable onPress={onPress} style={{
             padding: 14,
             marginTop: 100,
             backgroundColor: Colors.PRIMARY,
