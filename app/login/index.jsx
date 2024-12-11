a496d148c65dbee2174246432cb1748089f58f83
huascar <huascargutierrezcastro9951591@gmail.com>
2024-12-11 11:39:03 -0400

    constant color added by constants colors

--- app/login/index.jsx
+++ app/login/index.jsx
@@ -1,10 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import Colors from './../../constants/Colors'
 
 export default function LoginScreen() {
   return (
     <View>
-      <Text>LoginScreen</Text>
+        <Image source={require('./../../assets/images/login.png')} style={{
+            width: '100%',
+            height: 500,
+        }}/>
+        <View style={{
+            padding: 20,
+            display: 'flex',
+            alignItems: 'center',
+        }}>
+            <Text style={{
+            fontFamily: 'outfit',
+            fontSize: 25,
+            textAlign: 'center',
+        }}>Ready to make a new friend?</Text>
+        <Text style={{
+            fontWeight: 'lighter',
+            fontSize: 15,
+            textAlign: 'center',
+            color: Colors.GRAY,
+        }}>Let's adopt the pet which you like and make there life happy again</Text>
+        </View>
     </View>
   )
 }
