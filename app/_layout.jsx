d11a530b3b6f19fddc109d1509ac347b36b01468
huascar <huascargutierrezcastro9951591@gmail.com>
2024-12-11 11:25:23 -0400

    starting routes

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -13,6 +13,10 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index"/>
+      <Stack.Screen name="login/index"
+        options={{
+          headerShown: false,
+        }}/>
     </Stack>
   );
 }
